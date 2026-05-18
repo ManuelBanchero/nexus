914c168d1c90f63d3a696b26e6c961ca78a216d4
ManuelBanchero <manuelbanchero04@gmail.com>
2026-05-18 09:27:32 -0300

    notion parser done

--- backend/src_v2/domain/IParser.ts
+++ backend/src_v2/domain/IParser.ts
@@ -1,5 +1,5 @@
 import Page from './Page'
 
 export default interface IParser {
-    parse(id: string, title: string, content: string): Page
+    parse(id: string, content: string): Page
 }
---
+++ backend/src_v2/interface/NotionParser.ts
@@ -0,0 +1,44 @@
+import IParser from '../domain/IParser'
+import Page from '../domain/Page'
+
+export default class NotionParser implements IParser {
+    public parse(id: string, content: string): Page {
+        const blocks: string[] = this.extractPageBlocks(content)
+
+        return {
+            id,
+            title: this.extractTitleFromBlocks(blocks),
+            content,
+            url: this.getPageUrl(id),
+            childrenIds: this.extractChildrenIds(blocks),
+            keywords: []
+        }
+    }
+
+    private extractPageBlocks(content: string): string[] {
+        return content.split('\n\n')
+    }
+
+    private extractTitleFromBlocks(blocks: string[]): string {
+        return blocks[0].slice(1).trim()
+    }
+
+    private getPageUrl(id: string): string {
+        return `https://www.notion.so/${id}`
+    }
+
+    private extractChildrenIds(blocks: string[]): string[] {
+        const linkRegex = /(?<!\!)\[(.*?)\]\((.*?)\)/
+
+        // The id is an 32 chars hex, and is before the .md
+        const idRegex = /([a-f0-9]{32})\.md/i
+
+        return blocks
+            .filter(block => linkRegex.test(block))
+            .map(block => {
+                const match = block.match(idRegex)
+                return match ? match[1] : null
+            })
+            .filter((id): id is string => id !== null)
+    }
+}
